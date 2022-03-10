a37c4f2617e5f6245096d78029d1f2240ad14c1d
Steven James (he/him) <91357724+stevenjames-turing@users.noreply.github.com>
2022-03-10 14:57:30 -0600

    Feat: ChucService can find a random Chuck Norris joke

--- api_consumption/lib/chuck_service.rb
+++ api_consumption/lib/chuck_service.rb
@@ -8,7 +8,10 @@ class ChuckService
     Faraday.new("https://api.chucknorris.io")
   end
 
- 
+  def random
+    resp = @conn.get("/jokes/random")
+    json = JSON.parse(resp.body, sybolize_names: false)
+  end
 
 
 end
