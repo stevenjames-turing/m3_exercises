6b1e812ff0bc275adfb5351a95a8e3b6d6fbdff2
Steven James (he/him) <91357724+stevenjames-turing@users.noreply.github.com>
2022-03-11 12:49:10 -0600

    MusixMatch can find list of all genres

--- api_consumption/lib/musix_match_service.rb
+++ api_consumption/lib/musix_match_service.rb
@@ -1,5 +1,15 @@
 class MusixMatchService 
   def initialize
-    
+    @conn = connect
+    @api_key = "a57caa0e6b229a901ff32a3d652e0423"
+  end
+
+  def connect 
+    Faraday.new("https://api.musixmatch.com/ws/1.1/")
+  end
+
+  def music_genres 
+    resp = @conn.get("music.genres.get?apikey=#{@api_key}")
+    json = JSON.parse(resp.body, sybolize_names: false)
   end
 end
