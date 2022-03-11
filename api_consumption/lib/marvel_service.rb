783762a1e2bda8356f65459aa6ada0ed4665b8ae
Steven James (he/him) <91357724+stevenjames-turing@users.noreply.github.com>
2022-03-11 13:34:55 -0600

    Feat: MarvelService can get character info by name

--- api_consumption/lib/marvel_service.rb
+++ api_consumption/lib/marvel_service.rb
@@ -1,5 +1,17 @@
 class MarvelService 
   def initialize
-    
+    @conn = connect 
+    @public_key = "e77405aa0b1a98814cb6169c8beba643"
+    @md5 = "6fe48b1161201a879022002cc4a0d4b2"
   end
+  def connect 
+    Faraday.new("https://gateway.marvel.com/")
+  end
+
+  def character(name)
+    resp = @conn.get("/v1/public/characters?name=#{name}&ts=1&apikey=#{@public_key}&hash=#{@md5}")
+    json = JSON.parse(resp.body, symbolize_names: false)
+  end
+
+
 end
