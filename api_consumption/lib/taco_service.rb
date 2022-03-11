c2d1d1767000cce56b0c96533a6f17148e7d4d9c
Steven James (he/him) <91357724+stevenjames-turing@users.noreply.github.com>
2022-03-11 12:26:32 -0600

    TacoService can get a random taco

--- api_consumption/lib/taco_service.rb
+++ api_consumption/lib/taco_service.rb
@@ -1,5 +1,16 @@
 class TacoService
   def initialize
-    
+    @conn = connect
   end
+
+  def connect
+    Faraday.new("http://taco-randomizer.herokuapp.com")
+  end
+
+  # def random_ingredients
+  #   resp = @conn.get("/random")
+  #   json = JSON.parse(resp.body, symbolize_names: false)
+  # end
+
+  
 end
